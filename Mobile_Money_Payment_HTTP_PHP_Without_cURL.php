2ff6ceb2cabfa01552d3c1fbf09bb4acdec07dae
Winfred Worlanyo Adrah <worla@users.noreply.github.com>
2018-03-03 15:01:33 +0000

    Update Mobile_Money_Payment_HTTP_PHP_Without_cURL.php

--- Mobile_Money_Payment_HTTP_PHP_Without_cURL.php
+++ Mobile_Money_Payment_HTTP_PHP_Without_cURL.php
@@ -14,7 +14,7 @@ $url = "https://app.mpowerpayments.com/api/v1/direct-mobile/charge";
 $opts = array('http' =>
     array(
         'method'  => 'POST',
-        'header'  => "Content-Type: application/json\r\n" . "MP-Master-Key: arj67js7-g45n-1o1k-099t-5s7d8g8g0a73nx\r\n" . "MP-Private-Key: live_private_Hri2vBoqen5alM9c_4IvbbBqweersdFGH\r\n" . "MP-Token: 123asdt4ki85g12m9084r\r\n",
+        'header'  => "Content-Type: application/json\r\n" . "MP-Master-Key: arjx\r\n" . "MP-Private-Key: liveFGH\r\n" . "MP-Token: 12m9084r\r\n",
         'content' => json_encode($data)
     )
 );
